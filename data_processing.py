54ab8b75f170b5226279d4edbdf5eca41ba17a46
Vittorio Graglia <29167928+v-graglia@users.noreply.github.com>
2024-07-04 23:26:24 +0200

    Optimize data processing script
    - Define constants and utility functions for file creation time retrieval and date comparison.
    - Check for existing processed data to avoid redundant processing.
    - Streamline main process flow and error handling.

--- data_processing.py
+++ data_processing.py
@@ -2,6 +2,7 @@ import urllib3
 import zipfile
 import os
 import pandas as pd
+from datetime import datetime
 from bs4 import BeautifulSoup
 from io import BytesIO
 import requests
@@ -17,6 +18,26 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
 }
 
+JSON_FILENAME = 'data.json'
+
+def get_data_creation_time(filePath):
+    if not os.path.exists(filePath):
+        return None
+    
+    creation_time = None
+    
+    with open(filePath, 'r') as file:
+        data = json.load(file)        
+        try:
+            creation_time = data['creation_time']
+        
+        except:
+            return None    
+    return creation_time
+    
+def compare_dates(date1, date2):
+    return datetime.strptime(date1, '%Y-%m-%d') >= datetime.strptime(date2, '%Y-%m-%d')    
+    
 def update_readme_date(filepath, new_date):
     with open(filepath, 'r') as file:
         content = file.read()
@@ -34,7 +55,7 @@ def update_readme_date(filepath, new_date):
         # Write the updated content back to the file
         with open(filepath, 'w') as file:
             file.write(updated_content)
-        
+            print(f"README.md updated with date: {new_date}")
 
 def extract_filename_date(filename):
     pattern = r'(\d{4})(\d{2})(\d{2})'
@@ -79,6 +100,7 @@ def fetch_data():
 def process_data(zip_content):
     tempFolder = 'tmp'
     csvTime = None
+    list_cantieri = None
     
     if not os.path.exists(tempFolder):
         os.makedirs(tempFolder)
@@ -96,7 +118,13 @@ def process_data(zip_content):
 
     if not csvpath:
         raise Exception("No CSV Found!")
-        
+    
+    jsonCreationTime = get_data_creation_time(JSON_FILENAME)    
+    
+    if jsonCreationTime is not None and compare_dates(jsonCreationTime, csvTime):
+        print("Data already processed. Exiting...")
+        return jsonCreationTime, None
+    
     # Load only necessary columns
     columns = ['PROVINCIA', 'COMUNE', 'LATITUDINE', 'LONGITUDINE', 'CENTRALE_TX_DI_RIF', 'ID_ELEMENTO',
                'TIPO', 'TIPOLOGIA_CRO', 'STATO', 'INDIRIZZO', 'DATA_PUBBLICAZIONE']
@@ -112,15 +140,22 @@ def process_data(zip_content):
 if __name__ == "__main__":
     zip_content = fetch_data()
     
-    if zip_content is not None:
-        creation_time, data = process_data(zip_content)
-        # Save data to a JSON file
-        print("Saving data to JSON file")
-        with open('data.json', 'w') as f:
+    if zip_content is None:
+        print("Error downloading data. Exiting...")
+        exit()
+        
+    creation_time, data = process_data(zip_content)
+    
+    if creation_time is None or data is None:
+        exit()
+    
+    # Save data to a JSON file
+    print("Saving data to JSON file")
+    with open(JSON_FILENAME, 'w') as f:
+        
+        json.dump({"creation_time": creation_time, "data": data}, f)
+        print("Data saved to JSON file")
+        
+    if creation_time is not None:
+        update_readme_date('README.md', creation_time)
             
-            json.dump({"creation_time": creation_time, "data": data}, f)
-            print("Data saved to JSON file")
-            
-        if creation_time is not None:
-            update_readme_date('README.md', creation_time)
-            print("README.md updated")
